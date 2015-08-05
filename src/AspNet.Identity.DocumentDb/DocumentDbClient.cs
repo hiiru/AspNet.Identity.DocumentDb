4887a5af391f06e31247fc5e45492b357da37688
Hiiru <hiiru@rulez.ch>
2015-08-05 20:25:54 +0200

    initial documentdb implementaion

--- src/AspNet.Identity.DocumentDb/DocumentDbClient.cs
+++ src/AspNet.Identity.DocumentDb/DocumentDbClient.cs
@@ -14,8 +14,8 @@ namespace AspNet.Identity.DocumentDb
     public class DocumentDbClient
     {
         private readonly DocumentClient _client;
-        private readonly Database _db;
-        private readonly DocumentCollection _collection;
+        private Database _db;
+        private DocumentCollection _collection;
 
         public DocumentDbClient(IConfiguration config)
         {
@@ -28,30 +28,73 @@ namespace AspNet.Identity.DocumentDb
             if (string.IsNullOrWhiteSpace(url) || string.IsNullOrWhiteSpace(key) || string.IsNullOrWhiteSpace(db) || string.IsNullOrWhiteSpace(collection))
                 throw new ArgumentException(nameof(config));
             _client = new DocumentClient(new Uri(url), key, new ConnectionPolicy { ConnectionMode = ConnectionMode.Direct, ConnectionProtocol = Protocol.Tcp });
-            //EnsureDbSetup(db, collection);
-            //EnsureSpSetup();
+            EnsureDbSetup(db, collection);
         }
+
+        public DocumentDbClient(Uri endpoint, string key, string db = "AspNetIdentity", string collection = "AspNetIdentity")
+        {
+            if (endpoint == null)
+                throw new ArgumentNullException(nameof(endpoint));
+            if (key == null)
+                throw new ArgumentNullException(nameof(endpoint));
+            _client = new DocumentClient(endpoint, key, new ConnectionPolicy { ConnectionMode = ConnectionMode.Direct, ConnectionProtocol = Protocol.Tcp });
+            EnsureDbSetup(db, collection);
+        }
+
+        private async void EnsureDbSetup(string dbId, string collectionId)
+        {
+            _db = _client.CreateDatabaseQuery().Where(x => x.Id == dbId).AsEnumerable().FirstOrDefault();
+            if (_db == null)
+                _db = await _client.CreateDatabaseAsync(new Database { Id = dbId });
+            _collection = _client.CreateDocumentCollectionQuery(_db.CollectionsLink).Where(x => x.Id == collectionId).AsEnumerable().FirstOrDefault();
+            if (_collection == null)
+                _collection = await _client.CreateDocumentCollectionAsync(_db.CollectionsLink, new DocumentCollection { Id = collectionId });
+        }
+
+        public string UserPrefix { get; set; } = "user_";
+        public string RolePrefix { get; set; } = "role_";
+
         #region IdentityUser
-
-        internal Task UserAdd<TUser, TKey>(TUser user)
-            where TUser : IdentityUser<TKey>
-            where TKey : IEquatable<TKey>
+        
+        private string GetUserKey<TKey>(TKey userId)
         {
-            throw new NotImplementedException();
+            if (userId.Equals(default(TKey)))
+                return null;
+            return string.Concat(UserPrefix, userId);
         }
 
-        internal Task UserDelete<TUser, TKey>(TUser user)
+        internal async Task UserAdd<TUser, TKey>(TUser user)
             where TUser : IdentityUser<TKey>
             where TKey : IEquatable<TKey>
         {
-            throw new NotImplementedException();
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+            if (user.DocSelfLink != null || user.DocId != null)
+                throw new ArgumentException(nameof(user));
+            user.DocId = GetUserKey(user.DocId);
+            await _client.CreateDocumentAsync(_collection.DocumentsLink, user);
         }
 
-        internal Task UserUpdate<TUser, TKey>(TUser user)
+        internal async Task UserDelete<TUser, TKey>(TUser user)
             where TUser : IdentityUser<TKey>
             where TKey : IEquatable<TKey>
         {
-            throw new NotImplementedException();
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+            if (user.DocSelfLink == null || user.DocId == null)
+                throw new ArgumentException(nameof(user));
+            await _client.DeleteDocumentAsync(user.DocSelfLink);
+        }
+
+        internal async Task UserUpdate<TUser, TKey>(TUser user)
+            where TUser : IdentityUser<TKey>
+            where TKey : IEquatable<TKey>
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+            if (user.DocSelfLink == null || user.DocId == null)
+                throw new ArgumentException(nameof(user));
+            await _client.ReplaceDocumentAsync(user.DocSelfLink, user);
         }
 
         public async Task<IEnumerable<TUser>> UserSearch<TUser, TKey>(Expression<Func<TUser, bool>> predicate)
@@ -70,25 +113,40 @@ namespace AspNet.Identity.DocumentDb
         {
             return _client.CreateDocumentQuery<TUser>(_collection.DocumentsLink);
         }
-        #endregion
+
+        #endregion IdentityUser
+
         #region IdentityRole
 
-        internal Task RoleAdd<TRole>(TRole user) where TRole : IdentityRole
+        internal async Task RoleAdd<TRole>(TRole role) where TRole : IdentityRole
         {
-            throw new NotImplementedException();
+            if (role == null)
+                throw new ArgumentNullException(nameof(role));
+            if (role.DocSelfLink != null || role.DocId != null)
+                throw new ArgumentException(nameof(role));
+            role.DocId = string.Concat(RolePrefix, role.Name);
+            await _client.CreateDocumentAsync(_collection.DocumentsLink, role);
         }
 
-        internal Task RoleDelete<TRole>(TRole user) where TRole : IdentityRole
+        internal async Task RoleDelete<TRole>(TRole role) where TRole : IdentityRole
         {
-            throw new NotImplementedException();
+            if (role == null)
+                throw new ArgumentNullException(nameof(role));
+            if (role.DocSelfLink == null || role.DocId == null)
+                throw new ArgumentException(nameof(role));
+            await _client.DeleteDocumentAsync(role.DocSelfLink);
         }
 
-        internal Task RoleUpdate<TRole>(TRole user) where TRole : IdentityRole
+        internal async Task RoleUpdate<TRole>(TRole role) where TRole : IdentityRole
         {
-            throw new NotImplementedException();
+            if (role == null)
+                throw new ArgumentNullException(nameof(role));
+            if (role.DocSelfLink == null || role.DocId == null)
+                throw new ArgumentException(nameof(role));
+            await _client.ReplaceDocumentAsync(role.DocSelfLink, role);
         }
 
-        public async Task<IEnumerable<TRole>> RoleSearch<TRole>(Expression<Func<TRole, bool>> predicate)  where TRole : IdentityRole
+        public async Task<IEnumerable<TRole>> RoleSearch<TRole>(Expression<Func<TRole, bool>> predicate) where TRole : IdentityRole
         {
             if (predicate == null)
                 throw new ArgumentNullException(nameof(predicate));
@@ -100,6 +158,7 @@ namespace AspNet.Identity.DocumentDb
         {
             return _client.CreateDocumentQuery<TRole>(_collection.DocumentsLink);
         }
-        #endregion
+
+        #endregion IdentityRole
     }
 }
