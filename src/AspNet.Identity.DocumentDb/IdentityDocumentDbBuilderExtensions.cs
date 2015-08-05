908f3e1cee20a064d8c4ccb6643bb582e1d49da6
hiiru <hiiru@rulez.ch>
2015-08-05 17:57:01 +0200

    naming fixes

--- src/AspNet.Identity.DocumentDb/IdentityDocumentDbBuilderExtensions.cs
+++ src/AspNet.Identity.DocumentDb/IdentityDocumentDbBuilderExtensions.cs
@@ -6,18 +6,18 @@ namespace AspNet.Identity.DocumentDb
 {
     public static class IdentityDocumentDbBuilderExtensions
     {
-        public static IdentityBuilder AddEntityFrameworkStores<TContext>(this IdentityBuilder builder)
-            where TContext : DocumentDbClient
+        public static IdentityBuilder AddDocumentDbStores<TDocumentDb>(this IdentityBuilder builder)
+            where TDocumentDb : DocumentDbClient
         {
-            builder.Services.TryAdd(IdentityDocumentDbServices.GetDefaultServices(builder.UserType, builder.RoleType, typeof(TContext)));
+            builder.Services.TryAdd(IdentityDocumentDbServices.GetDefaultServices(builder.UserType, builder.RoleType, typeof(TDocumentDb)));
             return builder;
         }
 
-        public static IdentityBuilder AddEntityFrameworkStores<TContext, TKey>(this IdentityBuilder builder)
-            where TContext : DocumentDbClient
+        public static IdentityBuilder AddDocumentDbStores<TDocumentDb, TKey>(this IdentityBuilder builder)
+            where TDocumentDb : DocumentDbClient
             where TKey : IEquatable<TKey>
         {
-            builder.Services.TryAdd(IdentityDocumentDbServices.GetDefaultServices(builder.UserType, builder.RoleType, typeof(TContext), typeof(TKey)));
+            builder.Services.TryAdd(IdentityDocumentDbServices.GetDefaultServices(builder.UserType, builder.RoleType, typeof(TDocumentDb), typeof(TKey)));
             return builder;
         }
     }
